56e2317718030edc2ea4cfedf9f21ef238392a22
Demian Chidi <demianchidi@gmail.com>
2023-01-03 20:14:34 +0100

    create simple counter programm with javascript

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-//practice with hypoternus of a right angle triangle
+//practice create simple counter programm with javascript
+let count = 0;
 
+document.getElementById("decreaseBtn").onclick = function(){
+  count -= 1;
+  document.getElementById('count-label').innerHTML = count
+}
 
-//below is with window prompt
-// let a;
-// let b;
-// let c;
+document.getElementById("resetBtn").onclick = function(){
+  count = 0;
+  document.getElementById('count-label').innerHTML = count
+}
 
-// a = window.prompt("Enter Side A");
-// b = window.prompt("Enter Side B");
-
-// c = Math.pow(a, 2) + Math.pow(b, 2);
-
-// c = Math.sqrt(c);
-
-// //you can also put all c in one code
-// // c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-
-// console.log(c)
-
-
-
-// now with html input
-
-document.getElementById("button").onclick = function(){
-  a = document.getElementById("aside-textbox").value;
-  a = Number(a)
-
-  b = document.getElementById("bside-textbox").value;
-  b = Number(b)
-
-  c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-
-  document.getElementById("side-c").innerHTML = "Side C Is "+c;
-};
+document.getElementById("increaseBtn").onclick = function(){
+  count += 1;
+  document.getElementById('count-label').innerHTML = count
+}
