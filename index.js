a9da79b4f37b182b447ff2c8232d080c7aff0c30
Demian Chidi <demianchidi@gmail.com>
2023-01-03 21:11:47 +0100

    slice() method in javascript

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// useful string methods and properties in Javascript
+// slice method: extracts a section of a string and returns it as a new string, without modifying the original string.
 
 let fullName = "Demian Chidi";
-let phoneNumber = '123-456-3345'
+let firstName;
+let lastName;
 
-let nameLength = fullName.length;//to get the length of the string
-console.log(nameLength);
 
-console.log(fullName.charAt(4)); //getting a letter from a variable with index
-console.log(fullName.indexOf("C"));// to find the characters through index
-console.log(fullName.lastIndexOf("i"));// to find the last index of a character
+// firstName = fullName.slice(0, 6);
+// lastName = fullName.slice(7);
 
-fullName.trim(); //to remove any spaces before and after
+//we can do this same thing with indexof
 
-fullName.toUpperCase();//to turn oall the letters to uppercase
-fullName.toLowerCase(); // to turn all the characters to lowercase.
+lastName = fullName.slice(fullName.indexOf(" ")+1);
 
-phoneNumber = phoneNumber.replaceAll("-", "/");//replacing characters with another character , only work on strings
-console.log(phoneNumber);
+firstName = fullName.slice(0, fullName.indexOf(" "));
+
+
+
+console.log(firstName);
+console.log(lastName);
