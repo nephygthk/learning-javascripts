fa2c2be519ed473b09f9c9d2fa95a4c9ead95e6b
Demian Chidi <demianchidi@gmail.com>
2023-01-05 17:24:48 +0100

    break and continue statement in javascript[C

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-//For loop: repeat some codes a certian amount of times. in for loops you assign a variable, you check condition and you execute purpose of code in the bracket of the for loop. many programmers prefer to use "i" as a variable name for for loop.
+//break: breaks out of a loop entirely
+//continue: skip an interation of a loop
 
 
-for(let i = -1; i <= 10; i+=1){
+// this is example for break. it stops the iteration completely
+// for(let i = 1; i <= 20; i+=1){
+//   if(i == 15){
+//     break
+//   }
+//   console.log(i)
+// }
+
+
+
+//this is for continue, it skips the iteration and continue
+for(let i = 1; i <= 20; i+=1){
+  if(i == 15){
+    continue
+  }
   console.log(i)
 }
+
