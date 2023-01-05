327d4745fe8476efd3c9e622a8b5dc08856ab2ca
Demian Chidi <demianchidi@gmail.com>
2023-01-05 17:57:03 +0100

    nested loop in javascript

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-//break: breaks out of a loop entirely
-//continue: skip an interation of a loop
+// nested loop: it's a loop inside another loop. it is advicable to change the name of the counter variables when you are nesting a loop
 
-
-// this is example for break. it stops the iteration completely
-// for(let i = 1; i <= 20; i+=1){
-//   if(i == 15){
-//     break
-//   }
-//   console.log(i)
-// }
-
-
-
-//this is for continue, it skips the iteration and continue
-for(let i = 1; i <= 20; i+=1){
-  if(i == 15){
-    continue
+let symbol = window.prompt("Enter a symbol");
+let rows = window.prompt("Enter # of rows");
+let column = window.prompt("enter # columns");
+for(let i = 1; i <= rows; i+=1){
+  for(let j = 1; j <= column; j+=1){
+    document.getElementById("myRectangle").innerHTML += symbol;
   }
-  console.log(i)
+  document.getElementById("myRectangle").innerHTML += "<br>";
 }
-
