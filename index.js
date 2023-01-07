a0fb71f9c1faaf5101e2fdd4d1d6657d33917442
Demian Chidi <demianchidi@gmail.com>
2023-01-07 11:20:37 +0100

    Template reterals in javascript

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-// variable scope: where a variable is accessable.
-//let: the variables are limited to a block scope {}
-
-//var variables are limited to a function (){}
-
-//global variable: is declared outside any functions.(if global, var will change browser's window properties). using var keyword for declaring a global variable can be problematic because if a variable with var is declared global, it can and will change the browser window properties.
-
-//it's considered good practice to use let over var to avoid messing with your window property and causing you lots of problem because var can change a default window property with the same var name.
+// Template reterals: they are delimited with (`)  instead of double or single quotes. they allows embedded variables and expressions. it is like f string in python but you use ${} for the variable and change the "" to ``. it is really helpful when you want to update an html element with some text.
 
 
+let userName = "Nephyg";
+let items = 3;
+let total = 78;
 
-//var variable can escape {} while let variable cannot escape {},, var varaible can not escape a function (){}. escape here means can be accessable.
+// console.log("Hello", userName);
+// console.log("you bought",items, "items from us.");
+// console.log("your total prize is $",total );
 
-function doSomething(){
-  for(var i = 1; i<=3; i++){
-  
-  }
-}
+//to do this same thing with template reterals
 
-console.log(i);
+// console.log(`hello ${userName}`);
+// console.log(`You bought ${items} items from us.`);
+// console.log(`your total prize is $${total}`);
+
+// another example with long text is:
+
+let text = `hello ${userName}<br>
+You bought ${items} items from us.<br>
+your total prize is $${total}`
+
+//you can add it line break too
+
+// console.log("text")
+document.getElementById("myLabel").innerHTML = text;
