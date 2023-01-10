88e8b8f514705d964b32293f1f0a9cf71eb074b9
Demian Chidi <demianchidi@gmail.com>
2023-01-10 12:30:37 +0100

    function expression in javascript

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-// how to sort an array of numbers in javascript
+//function expression: it's a function without a name, (anonymous function).
+//avoid polluting the global scope with names 
+//Write it, then forget about it. it helps when you don't always want to be thinking about function names and also when you know that you won't be reusing the function.
 
-let grades = [100, 70,50,80,65];
+//you can store it in a variable if you want
 
-grades = grades.sort(ascending);
-console.log(grades); //prints an array
+// const greetings = function(){
+//   console.log("hello");
+// }
 
-grades.forEach(print);//prints individual elements
+// greetings();
+
+let count = 0;
+
+// function increaseCount(){
+//   count+=1;
+//   document.getElementById("myLabel").innerHTML = count
+// }
+// function decreaseCount(){
+//   count-=1;
+//   document.getElementById("myLabel").innerHTML = count
+// }
 
 
-function descending(x,y){
-  return y - x;
+//to do this same thing with function expression
+
+document.getElementById("increaseButton").onclick = function(){
+  count+=1;
+  document.getElementById("myLabel").innerHTML = count
 }
-
-function ascending(x,y){
-  return x - y;
-}
-
-function print(element){
-  console.log(element);
+document.getElementById("decreaseButton").onclick = function(){
+  count-=1;
+  document.getElementById("myLabel").innerHTML = count
 }
