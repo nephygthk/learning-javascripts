67102b8a214fc53b38f713c1cdb6fa4657cbedac
Demian Chidi <demianchidi@gmail.com>
2023-01-11 12:09:21 +0100

    anonymous objects in javascript

--- index.js
+++ index.js
@@ -1,73 +1,43 @@
-//array of objects
+// anonymous object: Object without a name
+//                  not directly reference
+//      less syntax, no need for unique names
 
 
-// how to pass objects to a function as an argument.
+class Card{
 
-//when you are passing an object to a function, the parameter name is kinda like a nick name you are giving it
-
-// class Car{
-
-//   constructor(model, year, color){
-//     this.model = model;
-//     this.year = year;
-//     this.color = color;
-//   }
-// }
-
-// const car1 = new Car("Camry", 2009, "yellow");
-// const car2 = new Car("Nissan", 2015, "black");
-// const car3 = new Car("BMW", 2018, "silver");
-
-// changeColor(car3, "blue");
-// displayInfo(car3);
-
-// function displayInfo(car){
-//   // the display must be the name of the class property
-//   console.log(car.model);
-//   console.log(car.year);
-//   console.log(car.color);
-// }
-
-// function changeColor(car, color){
-//   car.color = color;
-// }
-
-
-// how to create an array of objects in javascript
-
-class Car{
-
-  constructor(model, year, color){
-    this.model = model;
-    this.year = year;
-    this.color = color;
-  }
-
-  drive(){
-    console.log(`you are driving the ${this.color} ${this.model}`);
+  constructor(value, suit){
+    this.value = value;
+    this.suit = suit;
   }
 }
 
-const car1 = new Car("Camry", 2009, "yellow");
-const car2 = new Car("Nissan", 2015, "black");
-const car3 = new Car("BMW", 2018, "silver");
+// let card1 = new Card("A", "hearts");
+// let card2 = new Card("A", "spades");
+// let card3 = new Card("A", "diamond");
+// let card4 = new Card("A", "club");
+// let card5 = new Card("2", "hearts");
+// let card6 = new Card("2", "spades");
+// let card7 = new Card("2", "diamond");
+// let card8 = new Card("2", "club");
+
+// let cards = [card1,card2,card3,card4,card5,card6,card7,card8];
+
+//creating the same array with anonymous objects
 
 
-const cars = [car1, car2, car3];
+let cards = [new Card("A", "hearts"),
+            new Card("A", "spades"),
+            new Card("A", "diamond"),
+            new Card("A", "club"),
+            new Card("2", "hearts"),
+            new Card("2", "spades"),
+            new Card("2", "diamond"),
+            new Card("2", "club"), 
+];
 
-//to acess the object and the properties of this objects
-// console.log(cars[0].model);
-// console.log(cars[1].model);
+// console.log(card1.value + card1.suit);
+console.log(cards[0].value + cards[0].suit);
 
+//to diplay it with a foreach()
 
-//to access the method of an object within array
-// cars[2].drive();
-
-//to call the drive method of this class wiith a function and loop
-
-startRace(cars);
-function startRace(cars){
-  for(const car of cars){
-    car.drive();
-  }
-}
+cards.forEach(card => console.log(`${card.value}${card.suit}`));
