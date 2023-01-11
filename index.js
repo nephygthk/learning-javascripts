e2910521a1bf184ba75983b07eac2212f8604e75
Demian Chidi <demianchidi@gmail.com>
2023-01-11 14:10:30 +0100

    practice to create a clock that would update every second

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-// Date object: used to work with date and time
+//  practice to create a clock that would update every second
 
-// let date = new Date();
-// let date = new Date(0);// 0 is the reference point
-// let date = new Date(2023, 0, 1,14,23,00,);// year, month=> starts counting from 0, and then day. then hour, minutes and seconds.
+const myLabel = document.getElementById("myLabel");
 
-// let date = new Date("january 4, 2022, 0:00:00");// you can also pass a string representation of date
+update();
+setInterval(update, 1000)
+function update(){
+  let date = new Date();
+  myLabel.innerHTML = formatTime(date);
 
-let date = new Date();
+  function formatTime(date){
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let pmOrAm = hours >= 12 ? "pm" : "am";
 
-// let year = date.getFullYear();// to get year
-// let dayOfMonth = date.getDate();// to get date of the day
-// let dayOfWeek = date.getDay();// to get if it's monday to sunday. sunday is 0
+    hours = (hours % 12) || 12 ;// converts the hour from 24 to 12 type of hour
 
-// let month = date.getMonth(); //gets the month. january is 0
+    hours = formatZeroes(hours);
+    minutes = formatZeroes(minutes);
+    seconds = formatZeroes(seconds);
 
-// you can set aall this methods
+    return `${hours}:${minutes}:${seconds} ${pmOrAm}`
+  }
 
-// date.setFullYear("2025")
-
-// date = date.toLocaleString();
-
-console.log(formatTime(date));
-
-function formatDate(date){
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate()
-  return `${day}/${month}/${year}`;
+  function formatZeroes(time){
+    time = time.toString();
+    return time.length < 2 ? "0" + time : time; 
+  }
 }
-
-function formatTime(date){
-  let hour = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-
-  let amOrPm = hour >= 12 ? "pm" : "am";
-
-  return `${hour}:${minutes}:${seconds} ${amOrPm}`
-}
-
