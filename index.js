9cd08404aabcad5f969f7d5082e9aa6b0e9d965c
Demian Chidi <demianchidi@gmail.com>
2023-01-11 11:27:09 +0100

    getters and setters in Javascript get and set

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-// super: when using inheritance, it refers to the pparents class.
-// commonly used to invoke constructor of a parent class. it helps with code reusability
+//getters and setters.
 
-//instead of repeating some constructor properties in all your different classes. create it in parent class and call it with super() in child classes.
 
-class Animal{
+// get: binds an object property to a function, when that property is accesed. it increases data security and you can add some for objects to the get method.
 
-  constructor(name, age){
-    this.name = name;
-    this.age = age; 
+//note you can use the same name of the property as the get name, the best way to do it is to take name and preceed the property name with an undercore
+//this._power. note that doing this will make the property read only and not write at all
+//whenever you see _ before a property name, it means it is protected and shouldn't be messed with or changed.
+
+class Car{
+
+  constructor(power){
+    this._gas = 20;
+    this._power = power;
+  }
+
+  get power(){
+    return `${this._power}hp`;
+  }
+  get gas(){
+    return `${this._gas}L (${this._gas / 50 * 100}%)`;
+  }
+
+  set gas(value){
+    // to make sure users don't enter more value than i expect as maximum or less value that i expected
+    if(value > 50){
+      value = 50;
+    }
+    else if(value < 0){
+      value = 0;
+    }
+    this._gas = value;
   }
 }
-class Rabbit extends Animal{
-  constructor(name, age, runSpeed){
-    super(name,age);
-    this.runSpeed = runSpeed;
-  }
-}
 
-class Fish extends Animal{
-  constructor(name, age, swimSpeed){
-    super(name,age);
-    this.swimSpeed = swimSpeed;
-  }
-  
-}
+const car1 = new Car(400);
 
-class Hawk extends Animal{
-  constructor(name, age, flySpeed){
-    super(name,age);
-    this.flySpeed = flySpeed;
-  }
-}
+console.log(car1.power);
+// console.log(car1.gas);
 
-const fish = new Fish("fish",1,80);
 
-console.log(fish.name);
-
+// set: binds an object property to a function when that property is assigned a value. look at set gas function for example.
+car1.gas = -50;
+console.log(car1.gas);
