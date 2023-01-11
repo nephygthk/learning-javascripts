f386e9632d4c5f8753195ce295f2a8ba519b0c74
Demian Chidi <demianchidi@gmail.com>
2023-01-11 12:42:38 +0100

    setTimout in javascript

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-// error handling: object with a description of something that went wrong.
-// can't open a file
-// loose connection
-// user types incorrect inputs
-// type error
+// setTimeout: invokes a function after a number of miliseconds, asynchrounous function,(doesn't pause execution). 
 
-// one way to handle error is to put any dangerous code in try and catch which is the same with exception in python.
+// with setTimoue we pass in a callback, function expression or arrow function expression. it is possible to have multiple set timeout method executing concurrently. to clear your settimeout use clear method
+ let item = "Bag";
+ let price = 4500;
 
-//we use catch to catch the error or throw with if statement to write the error we want to be printed out
 
-//throw: executes a defined error. mostly used when things goes wrong but doesn't throw an error
+let timer1 = setTimeout(firstMessage, 3000, item, price);
+let timer2 = setTimeout(secondMessage, 6000);
+let timer3 = setTimeout(thirdMessage, 8000);
+//note every setTimeout returns an id.
 
-//finally: will always execute wether code is successful or not. mostly used to close a file. or do clean up.
-
-try{
-  let x = window.prompt("enter a number");
-  x = Number(x);
-
-  if(isNaN(x)) throw "That wasn't a number";
-
-  if(x == "") throw "You left the input empty, please enter a number";
-
-  console.log(`${x} is a number`);
+function firstMessage(item, price){
+  alert(`Buy this great ${item} for $${price}`);
 }
-catch(error){
-  console.log(error)
+function secondMessage(){
+  alert(`please buy it cus it's greate`);
 }
-finally{
-  console.log("file is closed")
+function thirdMessage(){
+  alert(`Buy it now!!`);
+}
+
+
+document.getElementById("myButton").onclick = function(){
+  clearTimeout(timer1);
+  clearTimeout(timer2);
+  clearTimeout(timer3);
+  alert(`Thank you for buying`);
 }
