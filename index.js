d7d05ad7cf7f35e18b3c36d846b480e7b2eee021
Demian Chidi <demianchidi@gmail.com>
2023-01-12 13:47:38 +0100

    DOM in javascript

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
+//Dom: Document Object Module (API)
+//    an ineterface for changing the content of a page.
 
-//Modules:
-// the idea behind a module is that it is a file of reusable code
-// we can impport sessions of prewritten codes to use whenever we want
-//great for any general utility values and functions
-// helps to make your code more reusable and maintainable
-// think of modules as seperate chapters of book
+// console.log(document);
+// console.dir(document);
+
+// console.log(document.title);
+// console.log(document.URL);
+
+// document.title = "Nephyg yoo";
 
 
-// import {PI, getCircumference, getArea} from "./math_util.js"
+// changing css of a dom
+document.body.style.backgroundColor = "black";
 
-//another way to import
-import * as MathUtil from "./math_util.js"
-console.log(MathUtil.PI);
-
-let circumference = MathUtil.getCircumference(10);
-console.log(circumference);
-
-let area = MathUtil.getArea(10);
-console.log(area);
+document.getElementById("myDiv").innerHTML = "hello";
