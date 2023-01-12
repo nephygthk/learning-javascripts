225ebbe010dad352b92bfd92db5de0e809b2f77e
Demian Chidi <demianchidi@gmail.com>
2023-01-12 09:28:50 +0100

    await keyword in javascript

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-// async: makes a function return a promise. when using async, there is no need to use resolve or rejected. if async is successful you will return something or it fails you use the throw keyword to throw an error 
-
-// const promise = new Promise((resolve, reject) => {
-//   let fileLoaded = true;
-//   if(fileLoaded){
-//     resolve("File is resolved");
-//   }
-//   else{
-//     reject("file is rejected");
-//   }
-
-// }); 
-
-// promise.then((value) => console.log(value))
-//       .catch(error => console.log(error));
-
-// doing this with async function. async replaces evrything about promise in the function.
+// await: makes a async function wait for a Promise. to be able to use await, you can only place await in async function for it to work. this is mostly used when you have to wait for multiple async functions.
 
 async function loadFile(){
   let fileLoaded = false;
@@ -26,25 +10,30 @@ async function loadFile(){
   }
 };
 
-loadFile().then((value) => console.log(value))
-      .catch(error => console.log(error));
 
-
-
-// another way without using async
-
-// function loadFile(){
-//   let fileLoaded = false;
-//   if(fileLoaded){
-//     return Promise.resolve("File is loaded");
-//   }
-//   else{
-//     return Promise.reject("file loading failed");
-//   }
-// };
-
+// await is used to replace this "then catch"
 // loadFile().then((value) => console.log(value))
 //       .catch(error => console.log(error));
+
+
+// to catch errors we round our await code in try and catch
+async function startProcess(){
+  try{
+    let message = await loadFile();
+    console.log(message);
+  }
+  catch(error){
+    console.log(error)
+  }
+  
+}
+
+startProcess();
+
+
+
+
+
 
 
 
