e0c347fad8ca0f0bbc510a5b37b9e6237a77105b
Demian Chidi <demianchidi@gmail.com>
2023-01-12 14:20:07 +0100

    how to select elements in javascript

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-//Dom: Document Object Module (API)
-//    an ineterface for changing the content of a page.
+// few different ways to select elements in a web page.
 
-// console.log(document);
-// console.dir(document);
+// let title = document.getElementById('myTitle');
 
-// console.log(document.title);
-// console.log(document.URL);
+// title.style.backgroundColor = "blue";
 
-// document.title = "Nephyg yoo";
+// let fruits = document.getElementsByName("fruits"); //this returns a node list, similar to array.
+
+// fruits.forEach(fruit => {
+//   if(fruit.checked){
+//     console.log(fruit.value);
+//   }
+// })
+
+// let soupIngredient = document.getElementsByTagName("li");
+
+// soupIngredient[3].style.backgroundColor = "blue";
+
+// let desserts = document.getElementsByClassName("desserts");
+// desserts[2].style.backgroundColor = "blue";
 
 
-// changing css of a dom
-document.body.style.backgroundColor = "black";
+//with queryselector you pass a name, id , classname or an attribute. you use # for id, . for classname, [for] for attributes like for in label
 
-document.getElementById("myDiv").innerHTML = "hello";
+// let element = document.querySelector("#myTitle"); // it selects the first elemnt of the group, if you need to select all you use querySelctorAll()
+// elements.style.backgroundColor = "yellow";
+
+let elements = document.querySelectorAll("[for]");
+elements.forEach(element => {
+  element.style.backgroundColor = "blue";
+})
+
+
