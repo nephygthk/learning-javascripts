4b42c509cf2ee0d391f9dc511b30493523cbf6c2
Demian Chidi <demianchidi@gmail.com>
2023-01-13 18:53:32 +0100

    creating few simple animations in javascript

--- index.js
+++ index.js
@@ -1,31 +1,79 @@
-//how to dictate key presses:
+// creating few simple animations using javascript.
 
-//keydoown is even listening property like click.
-const myDiv = document.getElementById("myDiv");
-window.addEventListener('keydown', move);
+const myButton = document.getElementById("myButton");
+const myAnimation = document.getElementById("myDiv");
 
-let x = 0;
-let y = 0;
+myButton.addEventListener("click", begin);
 
-function move (event){
-  switch(event.key){
-    case "ArrowDown":
-      y+=5;
-      myDiv.style.top = y + "px";
-      break
-    case "ArrowUp":
-      y-=5;
-      myDiv.style.top = y + "px";
-      break
-    case "ArrowRight":
-      x+=5;
-      myDiv.style.left = x + "px";
-      break
-    case "ArrowLeft":
-      x-=5;
-      myDiv.style.left = x + "px";
-      break
-    default:
-      break
+
+// function begin(){
+//   let timerId = null;
+//   let x = 0;
+//   let y = 0;
+
+//   timerId = setInterval(frame, 5)
+//   function frame(){
+//     if( x >=200 ){
+//       // myAnimation.style.display = "none";
+//       clearInterval(timerId);
+//     }
+//     else{
+//       x+=1;
+//       y+=1;
+//       myAnimation.style.left = x + "px";
+//       myAnimation.style.top = y + "px";
+//     }
+//   }
+// }
+
+
+function begin(){
+  let timerId = null;
+  let degres = 0;
+  let x = 0;
+  let y = 0;
+
+  timerId = setInterval(frame, 5)
+
+
+  function frame(){
+    if( x >= 300 || y >= 300){
+
+      clearInterval(timerId);
+    }
+    else{
+      degres+=4;
+      x+=1;
+      y+=1;
+
+      myAnimation.style.left = x + "px";
+      myAnimation.style.top = y + "px"
+      myAnimation.style.transform = "rotateZ("+degres+"deg)";
+
+    }
   }
 }
+
+
+
+// function begin(){
+//   let timerId = null;
+//   let scaleX = 1;
+//   let scaleY = 1;
+
+//   timerId = setInterval(frame, 10)
+
+
+//   function frame(){
+//     if( scaleX >= 3 || scaleY >= 3 ){
+
+//       clearInterval(timerId);
+//     }
+//     else{
+//       scaleX+=0.1;
+//       scaleY+=0.1;
+//       myAnimation.style.transform = "scale("+scaleX+", "+scaleY+")";
+
+//     }
+//   }
+// }
