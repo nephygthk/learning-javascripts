94cb810f23943e0eacb148ce9e5dca77c987b514
Demian Chidi <demianchidi@gmail.com>
2023-01-14 18:09:19 +0100

    cookie in javascript

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
-//window: interface used to talk with the web browser.
-//the DOM is property of the window
+// cookie: a small text file stored on your computer. used to remember information about the user.  saved in name= value pairs
 
-// console.dir(window);
+// console.log(navigator.cookieEnabled);// to check if cookie is anbled in your compueter
 
-// console.log(window.innerHeight);
-// console.log(window.innerWidth);
+// document.cookie = "firstName=smigo; expires=Sun, 1 january 2030 12:00:00 UTC; path=/" ;
+// document.cookie = "lastName=mike; expires=Sun, 1 january 2020 12:00:00 UTC; path=/" ;
 
-// console.log(window.scrollX);
-// console.log(window.scrollY);
 
-// console.log(window.location.href);// you can change the href to another url
+// setCookie("email", "smigo@gmail.com", 180);
+// deleteCookie("email");
+// deleteCookie("firstName");
+// console.log(document.cookie);
 
-// console.log(window.location.hostname);
+// setCookie("email", "smigo@gmail.com", 356);
+// setCookie("firstName", "Smigo", 180);
 
-//console.log(window.location.pathname);
+// console.log(getCookie("firstName"));
 
-let myButton = document.querySelector("#myButton");
 
-myButton.addEventListener("click", () => window.open("https://google.com"));// there is window.close(),window.print(), window.alert(),window.prompt(), window.confirm
+const firstText = document.querySelector("#firstText");
+const lastText = document.querySelector("#lastText");
+const submitBtn = document.querySelector("#submitBtn");
+const cookieBtn = document.querySelector("#cookieBtn");
+
+submitBtn.addEventListener("click", () =>{
+  setCookie("firstName", firstText.value, 365);
+  setCookie("lastName", lastText.value, 365);
+})
+cookieBtn.addEventListener("click", () =>{
+  firstText.value = getCookie("firstName");
+  lastText.value = getCookie("lastName");
+})
+
+
+function setCookie(name,value, daysToLive){
+  let date = new Date();
+  date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 100));
+  let expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+
+function deleteCookie(name){
+  setCookie(name, null, null);
+}
+
+function getCookie(name){
+  const cDecoded = decodeURIComponent(document.cookie);
+  const cArray = cDecoded.split("; ");
+  let result = null;
+  
+  cArray.forEach(element => {
+    if(element.indexOf(name) == 0){
+      result = element.substring(name.length + 1);
+    }   
+  })
+  return result;
+}
