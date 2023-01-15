a3fdbbf3f884569618665308657690986698e3fd
Demian Chidi <demianchidi@gmail.com>
2023-01-15 18:34:33 +0100

    building a snake game in javascript

--- index.js
+++ index.js
@@ -1,84 +1,176 @@
-// creating a tic tac toe game in javascript
+// building a snake game in javascrip[t]
 
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameheight = gameBoard.height; // this and width wouldn't have been available if we didn't assign them in our canvas html inline
 
-const cells = document.querySelectorAll(".cell");
-const statusText = document.querySelector("#statusText");
-const restartBtn = document.querySelector("#restartBtn");
-const winConditions = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-];
-
-let options = ["","","","","","","","",""];
-let currentPlayer = "X";
+const boardBackground = "white";
+const snakeColor = "purple";
+const snakeBorder = "black";
+const snakeFoodColor = "red";
+const unitSize = 15;
 let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
 
+let snake = [
+  {x:unitSize * 4, y:0},
+  {x:unitSize * 3, y:0},
+  {x:unitSize * 2, y:0},
+  {x:unitSize, y:0},
+  {x:0, y:0}
+]// this is an array of objects. that is ditionary in array
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
 
+gameStart();
 
-initializeGame();
-function initializeGame(){
-  cells.forEach(cell => cell.addEventListener("click", cellClicked));
-  restartBtn.addEventListener("click", restartGame);
-  statusText.textContent = `${currentPlayer}'s turn`;
+function gameStart(){
   running = true;
-}
-function cellClicked(){
-  const cellIndex = this.getAttribute("cellIndex");
-  if(options[cellIndex] != "" || !running){
-    return;
-  }
-  updateCell(this, cellIndex);
-  checkWinner();
-}
-function updateCell(cell, index){
-  options[index] = currentPlayer;
-  cell.textContent = currentPlayer;
-}
-function changePlayer(){
-  currentPlayer = (currentPlayer == "X") ? "O" : "X";
-  statusText.textContent = `${currentPlayer},s turn`;
-}
-function checkWinner(){
-  let roundWon = false;
+  scoreText.textContent = score;
+  createFood();
+  drawFood();
+  nextTick();
   
-  for(let i = 0; i < winConditions.length; i++){
-    const condition = winConditions[i];
-    const cellA = options[condition[0]];
-    const cellB = options[condition[1]];
-    const cellC = options[condition[2]];
-
-    if(cellA == "" || cellB == "" || cellC == ""){
-      continue;
-    }
-    else if(cellA == cellB && cellB == cellC){
-      roundWon = true;
-      break;
-    }
-  }
-  if(roundWon){
-    statusText.textContent = `${currentPlayer} wins!`;
-    running = false
-  }
-  else if(!options.includes("")){
-    statusText.textContent = `Draw!`;
-    running = false;
+};
+function nextTick(){
+  if(running){
+    setTimeout(() => {
+      clearBoard();
+      drawFood();
+      moveSnake();
+      drawSnake();
+      checkGameOver();
+      nextTick();
+    }, 100)
   }
   else{
-    changePlayer();
+    displayGameOver();
   }
-}
-function restartGame(){
-  currentPlayer = "X";
-  options = ["","","","","","","","",""];
-  statusText.textContent = `${currentPlayer}'s turn`;
-  cells.forEach(cell => cell.textContent = "");
-  running = true;
+};
+function clearBoard(){
+  ctx.fillStyle = boardBackground;
+  ctx.fillRect(0, 0, gameBoard.width, gameBoard.height);
+};
+function createFood(){
+  function randomFood(min, max){
+    const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+    return randNum;
+  }
+  foodX = randomFood(0, gameWidth - unitSize);
+  foodY = randomFood(0, gameWidth - unitSize);
+  
+};
+function drawFood(){
+  ctx.fillStyle = snakeFoodColor;
+  ctx.fillRect(foodX, foodY, unitSize, unitSize);
+};
+function moveSnake(){
+  const head = {x: snake[0].x + xVelocity,
+                y: snake[0].y + yVelocity};
 
-} 
-// restartBtn.addEventListener("click",restartGame );
+  snake.unshift(head);
+    //if food is eaten.
+  if(snake[0].x == foodX && snake[0].y == foodY ){
+    score+=1;
+    scoreText.textContent = score;
+    createFood();
+  }
+  else{
+    snake.pop();
+  }
+};
+function drawSnake(){
+  ctx.fillStyle = snakeColor;
+  ctx.strokeStyle = snakeBorder;
+  snake.forEach(snakePart => {
+    ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
+    ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
+  })
+};
+function changeDirection(event){
+  keyPressed = event.keyCode;
+  const LEFT = 37;
+  const UP = 38;
+  const RIGHT = 39;
+  const DOWN = 40;
+
+  const goingUp = (yVelocity == -unitSize)
+  const goingDown = (yVelocity == unitSize)
+  const goingRight = (xVelocity == unitSize)
+  const goingLeft = (xVelocity == -unitSize)
+
+  switch(true){
+
+    case(keyPressed == LEFT && !goingRight):
+      xVelocity = -unitSize;
+      yVelocity = 0;
+      break
+    case(keyPressed == UP && !goingDown):
+      xVelocity = 0;
+      yVelocity = -unitSize;
+      break
+    case(keyPressed == RIGHT && !goingLeft):
+      xVelocity = unitSize;
+      yVelocity = 0;
+      break
+    case(keyPressed == DOWN && !goingUp):
+      xVelocity = 0;
+      yVelocity = unitSize;
+      break
+      
+  }
+};
+function checkGameOver(){
+  switch(true){
+    case(snake[0].x < 0):
+      running = false;
+      displayGameOver();
+      break;
+    case(snake[0].x >= gameWidth):
+      running = false;
+      displayGameOver();
+      break;
+    case(snake[0].y < 0):
+      running = false;
+      displayGameOver();
+      break;
+    case(snake[0].y >= gameWidth):
+      running = false;
+      displayGameOver();
+      break;
+  }
+  //using this loop because we dont want to start from zero
+  for(let i = 1; i < snake.length; i+=1){
+    if(snake[i].x == snake[0].x && snake[i].y == snake[0].y ){
+      running = false;
+
+    }
+  }
+};
+function displayGameOver(){
+  ctx.font = "30px MV Boli";
+  ctx.fillStyle = "black";
+  ctx.textAlign = "center";
+  ctx.fillText("GAME OVER!", gameWidth / 2, gameheight / 2);
+  running = false;
+};
+function resetGame(){
+  score = 0;
+  xVelocity = unitSize;
+  yVelocity = 0;
+  snake = [
+    {x:unitSize * 4, y:0},
+    {x:unitSize * 3, y:0},
+    {x:unitSize * 2, y:0},
+    {x:unitSize, y:0},
+    {x:0, y:0}
+  ];
+  gameStart();
+};
